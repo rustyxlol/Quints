4ca44349b324de4b131cd6847f64e4d1a64f1335
Rusty <imkhazix@gmail.com>
2022-08-11 19:43:28 +0530

    Parse and obtain active runes

--- lcu_change_runes/parser/runes_reforged.py
+++ lcu_change_runes/parser/runes_reforged.py
@@ -5,12 +5,19 @@ from constants import URL_RUNES_REFORGED
 
 class RunesReforged:
     def __init__(self):
-        self.runes = {}
+        self.runes = {
+            5008: "The Adaptive Force Shard",
+            5005: "The Attack Speed Shard",
+            5007: "The Scaling CDR Shard",
+            5002: "The Armor Shard",
+            5003: "The Magic Resist Shard",
+            5001: "The Scaling Bonus Health Shard",
+        }
 
     def get_runes(self):
         return requests.get(URL_RUNES_REFORGED).json()
 
-    def parse_runes(self):
+    def parse_all_runes(self):
         all_runes = self.get_runes()
         for rune_path in all_runes:
             self.runes[rune_path["id"]] = rune_path["name"]
@@ -18,9 +25,17 @@ class RunesReforged:
                 for rune in runes["runes"]:
                     self.runes[rune["id"]] = rune["name"]
 
+    def map_to_id(self, raw_runes):
+        mapped_runes = {}
+        for rune in raw_runes:
+            for rune_id, rune_name in self.runes.items():
+                if rune_name in rune:
+                    mapped_runes[rune_id] = rune_name
+        return mapped_runes
+
 
 if __name__ == "__main__":
     runes = RunesReforged()
-    runes.parse_runes()
+    runes.parse_all_runes()
 
     print(runes.runes)
--- lcu_change_runes/parser/ugg.py
+++ lcu_change_runes/parser/ugg.py
@@ -7,6 +7,8 @@ from runes_reforged import RunesReforged
 
 
 class SParser:
+    """Parser for UGG"""
+
     def __init__(self, url):
         self.url = url
 
@@ -16,21 +18,19 @@ class SParser:
             executable_path=CHROME_DRIVER_PATH,
             options=self.options,
         )
-        self.runes = {}
-        self.active_runes = []
 
     def get_url_content(self):
         self.driver.get(self.url)
         return self.driver.page_source.encode("utf-8").strip()
 
-    def parse_url_content(self):
+    def get_active_runes(self):
         soup = BeautifulSoup(self.get_url_content(), "html.parser")
+        active_runes = []
 
         rune_container = soup.find(
             "div",
             class_="rune-trees-container-2 media-query media-query_MOBILE_LARGE__DESKTOP_LARGE",
         )
-
         rune_main_classes = [
             "rune-tree_v2 primary-tree",
             "secondary-tree",
@@ -43,40 +43,44 @@ class SParser:
         ]
 
         for (rune_class, rune_sub_class) in zip(rune_main_classes, rune_sub_classes):
-            self.active_runes.extend(
-                (self.get_active_runes(rune_container, rune_class, rune_sub_class))
+            active_runes.extend(
+                (self.parse_active_runes(rune_container, rune_class, rune_sub_class))
             )
-
-        self.map_runes_to_id()
-
-        self.stop_driver()
-
-    def get_active_runes(self, rune_container, rune_class, rune_sub_class):
-        active_runes = []
-
-        soup_all_runes = rune_container.find("div", class_=rune_class)
-        soup_active_runes = soup_all_runes.find_all("div", class_=rune_sub_class)
-
-        for rune in soup_active_runes:
-            active_runes.append(rune.img["alt"])
 
         return active_runes
 
-    def map_runes_to_id(self):
-        for rune in self.active_runes:
-            for rune_id, rune_name in Runes.runes.items():
-                if rune_name in rune:
-                    self.runes[rune_id] = rune_name
-        print(self.runes)
+    def parse_active_runes(self, rune_container, rune_class, rune_sub_class):
+        """Parse active runes from u.gg URL
+
+        Args:
+            rune_container: div containing rune information
+            rune_class: div containing primary rune information
+            rune_sub_class: div containing secondary rune information
+
+        Returns:
+            Active runes
+        """
+        parsed_active_runes = []
+
+        soup_all_runes = rune_container.find("div", class_=rune_class)
+        soup_parsed_active_runes = soup_all_runes.find_all("div", class_=rune_sub_class)
+
+        for rune in soup_parsed_active_runes:
+            parsed_active_runes.append(rune.img["alt"])
+
+        return parsed_active_runes
 
     def stop_driver(self):
+        """Stop the selenium driver"""
         self.driver.quit()
 
 
-url = "https://u.gg/lol/champions/nunu/build"
+if __name__ == "__main__":
+    url = "https://u.gg/lol/champions/nunu/build"
 
-Runes = RunesReforged()
-Runes.parse_runes()
+    Runes = RunesReforged()
+    Runes.parse_all_runes()
 
-ugg = SParser(url)
-ugg.parse_url_content()
+    ugg = SParser(url)
+    active_runes = Runes.map_to_id(ugg.get_active_runes())
+    ugg.stop_driver()
