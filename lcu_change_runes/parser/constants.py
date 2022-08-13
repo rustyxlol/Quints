6d7e914249154feeec1da17247ea1f271b69df48
Rusty <imkhazix@gmail.com>
2022-08-14 03:16:47 +0530

    added champions url

--- lcu_change_runes/parser/constants.py
+++ lcu_change_runes/parser/constants.py
@@ -1,3 +1,6 @@
 LEAGUE_VER = "12.15.1"
 CHROME_DRIVER_PATH = ".venv\chromedriver.exe"
 URL_RUNES_REFORGED = f"https://ddragon.leagueoflegends.com/cdn/{LEAGUE_VER}/data/en_US/runesReforged.json"
+URL_CHAMPIONS = (
+    f"http://ddragon.leagueoflegends.com/cdn/{LEAGUE_VER}/data/en_US/champion.json"
+)
